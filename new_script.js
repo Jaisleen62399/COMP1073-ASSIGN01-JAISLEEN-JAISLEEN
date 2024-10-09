5a2930a8b0fa3d2afd65fad0fce4f83f318308e7
Jaisleen62399 <156478290+Jaisleen62399@users.noreply.github.com>
2024-10-08 21:30:23 -0400

    Index variables

--- new_script.js
+++ new_script.js
@@ -3,3 +3,9 @@ const animalList = ["Lion", "Tiger", "Bear", "Shark", "Eagle"];
 const adverbList = ["Quickly", "Happily", "Carefully", "Aggressively", "Eagerly"];
 const actionList = ["Danced", "Slept", "Fought", "Sang", "Ran"];
 const locationList = ["In the jungle", "By the river", "On the mountain", "Under the stars", "Next to the volcano"];
+
+let adjIndex = 0;
+let animalIndex = 0;
+let adverbIndex = 0;
+let actionIndex = 0;
+let locationIndex = 0;

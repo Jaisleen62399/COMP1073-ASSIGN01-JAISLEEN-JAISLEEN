1b2d61f1e820567807bc78ad2cd2c0f106ece5b8
Jaisleen62399 <156478290+Jaisleen62399@users.noreply.github.com>
2024-10-08 21:45:45 -0400

    Update new_script.js

--- new_script.js
+++ new_script.js
@@ -9,3 +9,19 @@ let animalIndex = 0;
 let adverbIndex = 0;
 let actionIndex = 0;
 let locationIndex = 0;
+
+let isAdjSelected = false;
+let isAnimalSelected = false;
+let isAdverbSelected = false;
+let isActionSelected = false;
+let isLocationSelected = false;
+
+document.getElementById("adjButton").addEventListener("click", () => selectWord(1));
+document.getElementById("animalButton").addEventListener("click", () => selectWord(2));
+document.getElementById("adverbButton").addEventListener("click", () => selectWord(3));
+document.getElementById("actionButton").addEventListener("click", () => selectWord(4));
+document.getElementById("locationButton").addEventListener("click", () => selectWord(5));
+
+document.getElementById("createStoryBtn").addEventListener("click", createStory);
+document.getElementById("clearStoryBtn").addEventListener("click", resetStory);
+document.getElementById("surpriseMeBtn").addEventListener("click", randomStory);
